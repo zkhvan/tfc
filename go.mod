cadd0c096d8b101ec0962b64eeac03ff3e23bd80
Zhenya Khvan <zhenya.khvan@gmail.com>
2024-10-02 13:56:13 -0400

    feat: term struct representing an attached terminal

--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.23.0
 
 require (
 	github.com/MakeNowJust/heredoc v1.0.0
+	github.com/charmbracelet/colorprofile v0.1.1
 	github.com/hashicorp/go-tfe v1.67.1
 	github.com/sethvargo/go-envconfig v1.1.0
 	github.com/spf13/cobra v1.8.1
 )
 
 require (
+	github.com/charmbracelet/x/ansi v0.3.2 // indirect
+	github.com/charmbracelet/x/term v0.2.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
@@ -17,7 +20,10 @@ require (
 	github.com/hashicorp/go-version v1.7.0 // indirect
 	github.com/hashicorp/jsonapi v1.3.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
---
+++ pkg/term/term.go
@@ -0,0 +1,66 @@
+package term
+
+import (
+	"io"
+	"os"
+
+	"github.com/charmbracelet/colorprofile"
+)
+
+// Term represents information about the terminal that a process is connected
+// to.
+type Term struct {
+	in     io.Reader
+	out    io.Writer
+	errOut io.Writer
+
+	profile colorprofile.Profile
+}
+
+func New() Term {
+	t := Term{
+		in:     os.Stdin,
+		out:    colorprofile.NewWriter(os.Stdout, os.Environ()),
+		errOut: colorprofile.NewWriter(os.Stderr, os.Environ()),
+	}
+
+	t.profile = colorprofile.Detect(t.out, os.Environ())
+
+	return t
+}
+
+// In is the reader reading from standard input.
+func (t Term) In() io.Reader {
+	return t.in
+}
+
+// Out is the writer writing to standard output.
+func (t Term) Out() io.Writer {
+	return t.out
+}
+
+// ErrOut is the writer writing to standard error.
+func (t Term) ErrOut() io.Writer {
+	return t.errOut
+}
+
+// IsTerminalOutput returns true if standard output is connected to a terminal.
+func (t Term) IsTerminalOutput() bool {
+	return t.profile != colorprofile.NoTTY
+}
+
+// IsColorEnabled reports whether it's safe to output ANSI color sequences,
+// depending on IsTerminalOutput and environment variables.
+func (t Term) IsColorEnabled() bool {
+	return t.profile != colorprofile.NoTTY && t.profile != colorprofile.Ascii
+}
+
+// Is256ColorSupported reports whether the terminal advertises ANSI 256 color codes.
+func (t Term) Is256ColorSupported() bool {
+	return t.profile == colorprofile.TrueColor || t.profile == colorprofile.ANSI256
+}
+
+// IsTrueColorSupported reports whether the terminal advertises support for ANSI true color sequences.
+func (t Term) IsTrueColorSupported() bool {
+	return t.profile == colorprofile.TrueColor
+}
