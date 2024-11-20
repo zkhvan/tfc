a83c02982eb5e1561c3b290e4cf76a7f607515a1
Zhenya Khvan <zhenya.khvan@gmail.com>
2024-11-20 15:02:15 -0500

    feat(ws/list): search by tag/exclude-tag

--- cmd/tfc/workspaces/list/list.go
+++ cmd/tfc/workspaces/list/list.go
@@ -64,6 +64,8 @@ type Options struct {
 	Organization      string
 	OrganizationExact bool
 	Name              string
+	Tags              []string
+	ExcludeTags       []string
 
 	// Filter the results based on various groups of run statuses.
 	Pending bool
@@ -118,6 +120,8 @@ func NewCmdList(f *cmdutil.Factory) *cobra.Command {
 
 	cmd.Flags().StringVarP(&opts.Name, "name", "n", "", "Search by the workspace name.")
 	cmd.Flags().StringVarP(&opts.Organization, "org", "o", "", "Search by the organization name.")
+	cmd.Flags().StringSliceVarP(&opts.Tags, "tags", "t", []string{}, "Search by the tags.")
+	cmd.Flags().StringSliceVarP(&opts.ExcludeTags, "exclude-tags", "T", []string{}, "Search by excluding the tags.")
 
 	cmd.Flags().BoolVar(&opts.Pending, "pending", false, "Search for workspaces with pending runs.")
 	cmd.Flags().BoolVar(&opts.Errored, "errored", false, "Search for workspaces with errored runs.")
@@ -185,9 +189,11 @@ func (opts *Options) Run(ctx context.Context) error {
 	for _, org := range orgs {
 		listOpts := &ListOptions{
 			Name:        opts.Name,
-			Limit:       limit,
+			Tags:        opts.Tags,
+			ExcludeTags: opts.ExcludeTags,
 			RunStatus:   opts.runStatus(),
 			IncludeRuns: slices.Contains(opts.Columns, ColumnRunStatus),
+			Limit:       limit,
 		}
 
 		workspaces, totalCount, err := listWorkspaces(ctx, client, org.Name, listOpts)
@@ -366,9 +372,12 @@ func filterOrganizations(orgs *tfe.OrganizationList, name string) *tfe.Organizat
 
 type ListOptions struct {
 	Name        string
-	Limit       int
+	Tags        []string
+	ExcludeTags []string
 	RunStatus   string
 	IncludeRuns bool
+
+	Limit int
 }
 
 func listWorkspaces(ctx context.Context, client *tfe.Client, org string, opts *ListOptions) ([]*tfe.Workspace, int, error) {
@@ -376,6 +385,8 @@ func listWorkspaces(ctx context.Context, client *tfe.Client, org string, opts *L
 		wsListOpts := &tfe.WorkspaceListOptions{
 			ListOptions:      lo,
 			Search:           opts.Name,
+			Tags:             strings.Join(opts.Tags, ","),
+			ExcludeTags:      strings.Join(opts.ExcludeTags, ","),
 			CurrentRunStatus: opts.RunStatus,
 		}
 
